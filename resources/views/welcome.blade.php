84b0c98d5ba258f2a0c10b59dd852383d1703602
Edrick Mar <emobual2019@plm.edu.ph>
2024-09-18 17:48:49 +0800

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body class="d-flex justify-content-center mt-5" >
+    <div class="card" style="width:497px">
+       
+        <div class="card-header">
+            Application Form
+        </div> 
+        
+        <div class="card-body d-flex justify-content-center">
+            <div class="row"> 
+                
+                <!--FIRST NAME-->
+                <div class="col-12 mb-3">
+                    <label for="">First Name:</label>
+                    <input class="mx-3" type="text" name="" id="">
+                </div>
+
+                <!--LAST NAME-->
+                <div class="col-12">
+                    <label for="">Last Name:</label>
+                    <input class="mx-3" type="text" name="" id="">
+                </div>
+            </div>
+        </div>
+        
+        <!--FOOTER-->
+        <div class="card-footer">
+        <input class="btn btn-success" type="button" value="Submit">
+                
+        </div>
+
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
